c64736e630d9d5add123c9d96b76ece5f4aa273a
JackYI <feiyi0727@gmail.com>
2021-11-12 14:22:48 +0100

    gg

---
+++ EudcationEnfant/Assets/Scripts/AbilityController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityController : MonoBehaviour
+{
+    public enum  Ability
+    {
+        FineMotor =0,
+        GrossMotor=1,
+        Cognition =2,
+        Sensory=3,
+        Language=4,
+        Social=5,
+        Affective=6,
+        Math=7,
+        Lecture=8,
+        Ecriture=9,
+        Dessin=10
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
